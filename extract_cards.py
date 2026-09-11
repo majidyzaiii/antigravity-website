e5c12cc9360052c368e1f2f597517c9206da9450
unknown <majidyzaiii@gmail.com>
2026-09-11 05:45:25 +0500

    Implement Updates page layout with text-only announcement containers

--- extract_cards.py
+++
@@ -1,6 +0,0 @@
-with open('index.html', 'r', encoding='utf-8') as f:
-    c = f.read()
-import re
-for m in re.finditer(r'<article class="news-card">.*?</article>', c, re.DOTALL):
-    print(m.group(0))
-    print('---')
--- extract_cards2.py
+++
@@ -1,7 +0,0 @@
-with open('index.html', 'r', encoding='utf-8') as f:
-    c = f.read()
-import re
-with open('cards.txt', 'w', encoding='utf-8') as out:
-    for m in re.finditer(r'<article class="news-card">.*?</article>', c, re.DOTALL):
-        out.write(m.group(0))
-        out.write('\n---\n')
