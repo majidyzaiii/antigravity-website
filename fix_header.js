5887c0855959e71b33b40a265df2089da676d390
unknown <majidyzaiii@gmail.com>
2026-09-23 02:50:59 +0500

    chore: Clean up all temporary node scripts

--- fix_header.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-['contact.html', 'donation.html'].forEach(file => {
-    let html = fs.readFileSync(file, 'utf8');
-    html = html.replace('<body class="hide-main-header"', '<body');
-    fs.writeFileSync(file, html, 'utf8');
-    console.log('Fixed ' + file);
-});
